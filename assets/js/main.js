b92f1105c84071d2f13b1df0552607a6b5d2dcfb
Marco Inchingoli <evilmarkus@hotmail.it>
2022-11-08 15:12:13 +0100

    Initial Commit

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+/* Descrizione:
+Attraverso l'apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail generare 10 indirizzi email e stamparli in pagina all'interno di una lista
+
+
+Bonus
+Far comparire gli indirizzi email solamente quando sono stati tutti generati. */
+
+//Crea una chiamata Axion con la API della mail
+//Attraverso un ciclo for effettua 10 chiamate
+//Il valore andrà inserito nel markup in una ul
+
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            
+        }
+    },
+
+    methods: {
+
+    },
+    mounted() {
+
+    }
+}).mount('#app')
