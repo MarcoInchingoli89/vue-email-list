0563a79cced165238885c85aff3b0bdae282807d
Marco Inchingoli <evilmarkus@hotmail.it>
2022-11-08 15:28:02 +0100

    Add Axios For Cycle

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,7 @@ Attraverso l'apposita API di Boolean https://flynn.boolean.careers/exercises/api
 Bonus
 Far comparire gli indirizzi email solamente quando sono stati tutti generati. */
 
-//Crea una chiamata Axion con la API della mail
+//Crea una chiamata Axios con la API della mail
 //Attraverso un ciclo for effettua 10 chiamate
 //Il valore andrà inserito nel markup in una ul
 
@@ -22,6 +22,13 @@ createApp({
 
     },
     mounted() {
-
+        for (let index = 0; index < 10; index++) {
+            axios
+            .get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then(response => {
+                console.log(response);
+            })
+            
+        }
     }
 }).mount('#app')
